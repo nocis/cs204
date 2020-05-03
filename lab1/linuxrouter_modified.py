69628c146c86fbd5ac3d9316724a0d47744e2532
nocis <nocis@tju.edu.cn>
2020-05-03 01:08:00 -0700

    test

--- lab1/linuxrouter_modified.py
+++ lab1/linuxrouter_modified.py
@@ -87,9 +87,13 @@ def run():
     topo = NetworkTopo()
     net = Mininet( topo=topo )  # controller is used by s1-s3
     net['h1'].setIP( ip='10.0.1.100/24',
-                           intf='h1-eth1',defaultRoute='via 10.0.1.1' )
+                           intf='h1-eth1' )
+    net['h1'].setHostRoute(ip='10.0.1.100/24',
+                           intf='h1-eth1' )
     net['h2'].setIP( ip='10.0.3.100/24',
-                           intf='h2-eth1',defaultRoute='via 10.0.3.1' )
+                           intf='h2-eth1' )
+    net['h1'].setHostRoute(ip='10.0.3.100/24',
+                           intf='h2-eth1' )
     net.start()
     info( '*** Routing Table on Router:\n' )
     info( net[ 'r0' ].cmd( 'route' ) )
