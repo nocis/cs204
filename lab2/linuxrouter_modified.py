f017d009c8deee8c7b935995fb827e14f98c2180
nocis <nocis@tju.edu.cn>
2020-06-06 22:47:41 -0700

    test

--- lab2/linuxrouter_modified.py
+++ lab2/linuxrouter_modified.py
@@ -8,8 +8,6 @@ class MyTopo( Topo ):
 
         # Add hosts and switches
         h1 = self.addHost( 'h1' )
-        h2 = self.addHost( 'h2' )
-        h3 = self.addHost( 'h3' )
         h4 = self.addHost( 'h4' )
         h5 = self.addHost( 'h5' )
 
