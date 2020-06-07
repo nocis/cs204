6b19f9d84520d82677d6a1e834b78e3f2c88e067
nocis <nocis@tju.edu.cn>
2020-06-07 01:48:48 -0700

    test

--- lab2/topo_d.py
+++ lab2/topo_d.py
@@ -7,11 +7,11 @@ class MyTopo( Topo ):
         "Create custom topo."
 
         # Add hosts and switches
-        h1 = self.addHost( 'h1', ip='10.0.0.1/16' )
-        h2 = self.addHost( 'h2', ip='10.0.0.2/16' )
-        h3 = self.addHost( 'h3', ip='10.0.0.3/16' )
-        h4 = self.addHost( 'h4', ip='10.0.1.2/16' )
-        h5 = self.addHost( 'h5', ip='10.0.1.3/16' )
+        h1 = self.addHost( 'h1', ip='10.0.0.1/16', defaultRoute='via 10.0.0.1' )
+        h2 = self.addHost( 'h2', ip='10.0.0.2/16', defaultRoute='via 10.0.0.1' )
+        h3 = self.addHost( 'h3', ip='10.0.0.3/16', defaultRoute='via 10.0.0.1' )
+        h4 = self.addHost( 'h4', ip='10.0.1.2/16', defaultRoute='via 10.0.1.1' )
+        h5 = self.addHost( 'h5', ip='10.0.1.3/16', defaultRoute='via 10.0.1.1' )
 
         s1 = self.addSwitch( 's1' )
         s2 = self.addSwitch( 's2' )
